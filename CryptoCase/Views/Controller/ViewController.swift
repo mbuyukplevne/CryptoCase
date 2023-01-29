112c2e0af0a06ffb626a324c8ec5eb39e9d5eb08
Mehdican Buyukplevne <mbuyukplevne@hotmail.com>
2023-01-29 20:44:14 +0300

    second Commit, will desing SortButton !

--- CryptoCase/Views/Controller/ViewController.swift
+++ CryptoCase/Views/Controller/ViewController.swift
@@ -9,33 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sortButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
+    
+    var viewModel: CryptoViewModel?
+    var selectedTitle: String?
+    let menuView = UIView(frame: CGRect(x: 260, y: 100, width: 140, height: 150))
+    let priceButton
+    
+    
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+        
        
     }
     
-   
+    @IBAction func sortButtonTapped(_ sender: UIButton) {
+    }
+    
     
     
 }
 
-extension ViewController: UITableViewDelegate,UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoTableViewCell", for: indexPath) as! CryptoTableViewCell
-        
-        return cell
-    }
-}
+
 
